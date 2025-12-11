f90bc16697460d3da88cc642f26d288db9383edd
ChauhanS3010 <singhsubhadra1030@gmail.com>
2025-12-11 17:16:37 +0530

    Lab2,11-12-25

---
+++ Lab2/Lab2_5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Lab2_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter First Name: ");
+        String firstName = sc.nextLine();
+
+        System.out.print("Enter Last Name: ");
+        String lastName = sc.nextLine();
+
+        System.out.println(lastName + " " + firstName);
+    }
+}
